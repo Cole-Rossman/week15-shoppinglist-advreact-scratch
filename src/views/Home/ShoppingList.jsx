f010e8cd0428a0ff42f5d01fbacf893787ea0b4a
Cole Rossman <colerossman99@gmail.com>
2022-05-12 15:31:39 -0700

    list provider wrapped around app in index.jsx and shoppinglist function almost complete

--- src/views/Home/ShoppingList.jsx
+++ src/views/Home/ShoppingList.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import { useState } from 'react';
+import { useList } from '../../context/ListProvider';
+import Item from '../../components/Items/Item';
 
 export default function ShoppingList() {
+  const [newItem, setNewItem] = useState('');
+  const { items, handleAddItem, handleDeleteItem, handleUpdateItem } = useList();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    handleAddItem(newItem);
+    setNewItem('');
+  };
+
   return (
-    <div>ShoppingList</div>
+    <>
+    <h1>Shopping Items</h1>
+    <form onSubmit={handleSubmit}>
+      <input
+      type="text"
+      name="newItem"
+      placeholder="Enter new item"
+      value={newItem}
+      onChange={(event) => setNewItem(event.target.value)}
+      />
+    </form>
+    </>
   )
 }
